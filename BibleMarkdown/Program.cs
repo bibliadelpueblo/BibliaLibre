e6fa2adde98c244d94bce05858db204a403d23b9
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2020-08-30 18:30:41 +0200

    Initio

---
+++ BibleMarkdown/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions; 
+
+namespace BibleMarkdown
+{
+	class Program
+	{
+
+		static DateTime bibmarktime;
+		static bool LowercaseFirstWords = false;
+		static bool Force = false;
+	
+		static void ImportFromUSFM(string mdpath, string srcpath)
+		{
+			var sources = Directory.EnumerateFiles(srcpath)
+				.Where(file => file.EndsWith(".usfm"));
+
+			if (sources.Any())
+			{
+
+				var mdtimes = Directory.EnumerateFiles(mdpath)
+					.Select(file => File.GetLastWriteTimeUtc(file));
+				var sourcetimes = sources.Select(file => File.GetLastWriteTimeUtc(file));
+
+				var mdtime = DateTime.MinValue;
+				var sourcetime = DateTime.MinValue;
+
+
+				foreach (var time in mdtimes) mdtime = time > mdtime ? time : mdtime;
+				foreach (var time in sourcetimes) sourcetime = time > sourcetime ? time : sourcetime;
+
+				if (Force || mdtime < sourcetime)
+				{
+
+					int bookno = 1;
+
+					foreach (var source in sources)
+					{
+						var src = File.ReadAllText(source);
+						var bookm = Regex.Match(src, @"\\h\s+(.*?)$", RegexOptions.Multiline);
+						var book = bookm.Groups[1].Value.Trim();
+
+						src = Regex.Match(src, @"\\c\s+[0-9]+.*", RegexOptions.Singleline).Value; // remove header that is not content of a chapter
+						src = src.Replace("\r", "").Replace("\n", ""); // remove newlines
+						src = Regex.Replace(src, @"(?<=\\c\s+[0-9]+\s*(\\s[0-9]+\s+[^\\]*?)?)\\p", ""); // remove empty paragraph after chapter
+						src = Regex.Replace(src, @"\\m?s([0-9]+)\s*([^\\]+)", m =>
+						{
+							int n = 1;
+							int.TryParse(m.Groups[1].Value, out n);
+							n++;
+							return $"{new String('#', n)} {m.Groups[2].Value.Trim()}{Environment.NewLine}";
+						}); // section titles
+						bool firstchapter = true;
+						src = Regex.Replace(src, @"\\c\s+([0-9]+\s*)", m =>
+						{
+							var res = firstchapter ? $"# {m.Groups[1].Value}{Environment.NewLine}" : $"{Environment.NewLine}{Environment.NewLine}# {m.Groups[1].Value}{Environment.NewLine}";
+							firstchapter = false;
+							return res;
+						}); // chapters
+						src = Regex.Replace(src, @"\\v\s+([0-9]+)", "^$1^"); // verse numbers
+						src = Regex.Replace(src, @"\\p *", string.Concat(Enumerable.Repeat(Environment.NewLine, 2))); // replace new paragraph with empty line
+						src = Regex.Replace(src, @"\|(\w+=""[^""]*""\s*)+", ""); // remove word attributes
+						src = Regex.Replace(src, @"\\\+?\w+(\*|\s*)?", ""); // remove usfm tags
+						src = Regex.Replace(src, @" +", " "); // remove multiple spaces
+
+						if (LowercaseFirstWords) // needed for ReinaValera1909, it has uppercase words on every beginning of a chapter
+						{
+							src = Regex.Replace(src, @"(\^1\^ \w)(\w*)", m => $"{m.Groups[1].Value}{m.Groups[2].Value.ToLower()}");
+							src = Regex.Replace(src, @"(\^1\^ \w )(\w*)", m => $"{m.Groups[1].Value}{m.Groups[2].Value.ToLower()}");
+						}
+
+						var md = Path.Combine(mdpath, $"{bookno:D2}-{book}.md");
+						bookno++;
+						File.WriteAllText(md, src);
+						Console.WriteLine($"Created {md}.");
+					}
+
+				}
+
+			}
+		}
+
+		static void CreatePandoc(string file, string panfile)
+		{
+			var text = File.ReadAllText(file);
+			text = Regex.Replace(text, @"\^\*\^(.*?)(\^\[.*?\])", "$2$1", RegexOptions.Singleline);
+			if (text.Contains(@"%\verse-paragraphs")) // each verse in a separate paragraph. For use in Psalms & Proverbs
+			{
+				text = Regex.Replace(text, @"(\^[0-9]+\^[^#]*?)(\s*?)(?=\^[0-9]+\^)", "$1\\\n", RegexOptions.Singleline);
+			}
+
+			text = Regex.Replace(text, @"\[\](.*?)(\^\[.*?\])", "$2$1", RegexOptions.Singleline); // footnotes with []
+			text = Regex.Replace(text, @"\^([0-9]+)\^", @"\bibverse{$1}", RegexOptions.Singleline); // verses
+			text = Regex.Replace(text, @"^%.*?$", "", RegexOptions.Multiline); // comments
+			text = Regex.Replace(text, @"^(# .*?)$\n^(## .*?)$", "$2\n$1", RegexOptions.Multiline); // titles
+
+			/*
+			text = Regex.Replace(text, @" ^# (.*?)$", @"\chapter{$1}", RegexOptions.Multiline);
+			text = Regex.Replace(text, @"^## (.*?)$", @"\section{$1}", RegexOptions.Multiline);
+			text = Regex.Replace(text, @"^### (.*?)$", @"\subsection{$1}", RegexOptions.Multiline);
+			text = Regex.Replace(text, @"^#### (.*)$", @"\subsubsection{$1}", RegexOptions.Multiline);
+			text = Regex.Replace(text, @"\*\*(.*?)(?=\*\*)", @"\bfseries{$1}");
+			text = Regex.Replace(text, @"\*([^*]*)\*", @"\emph{$1}", RegexOptions.Singleline); 
+			text = Regex.Replace(text, @"\^\[([^\]]*)\]", @"\footnote{$1}", RegexOptions.Singleline);
+			*/
+
+			File.WriteAllText(panfile, text);
+			Console.WriteLine($"Created {panfile}.");
+		}
+
+		static void CreateTeX(string mdfile, string texfile)
+		{
+			var pandoc = new ProcessStartInfo("pandoc.exe", $"-f markdown -t latex -o \"{texfile}\" \"{mdfile}\"");
+			pandoc.CreateNoWindow = true;
+			pandoc.WindowStyle = ProcessWindowStyle.Hidden;
+			pandoc.RedirectStandardOutput = true;
+			pandoc.RedirectStandardError = true;
+			pandoc.UseShellExecute = false;
+			var process = Process.Start(pandoc);
+			process.WaitForExit();
+			Console.WriteLine(process.StandardOutput.ReadToEnd());
+			Console.WriteLine(process.StandardError.ReadToEnd());
+			Console.WriteLine($"Created {texfile}.");
+		}
+		static void ProcessFile(string file)
+		{
+			var path = Path.GetDirectoryName(file);
+			var md = Path.Combine(path, "pandoc");
+			var tex = Path.Combine(path, "tex");
+			if (!Directory.Exists(md)) Directory.CreateDirectory(md);
+			if (!Directory.Exists(tex)) Directory.CreateDirectory(tex);
+			var mdfile = Path.Combine(md, Path.GetFileName(file));
+			var texfile = Path.Combine(tex, Path.GetFileNameWithoutExtension(file) + ".tex");
+
+			var mdfiletime = DateTime.MinValue;
+			var texfiletime = DateTime.MinValue;
+			var filetime = File.GetLastWriteTimeUtc(file);
+
+			if (File.Exists(mdfile)) mdfiletime = File.GetLastWriteTimeUtc(mdfile);
+			if (mdfiletime < filetime || mdfiletime < bibmarktime)
+			{
+				CreatePandoc(file, mdfile);
+				mdfiletime = DateTime.Now;
+			}
+
+			if (File.Exists(texfile)) texfiletime = File.GetLastWriteTimeUtc(texfile);
+			if (texfiletime < mdfiletime || texfiletime < bibmarktime)
+			{
+				CreateTeX(mdfile, texfile);
+			}
+		}
+
+		static void ProcessPath(string path)
+		{
+			var srcpath = Path.Combine(path, "src");
+			ImportFromUSFM(path, srcpath);
+			var files = Directory.EnumerateFiles(path, "*.md");
+			foreach (var file in files) ProcessFile(file);
+		}
+		static void Main(string[] args)
+		{
+
+			// Get the version of the current application.
+			var asm = Assembly.GetExecutingAssembly();
+			var aname = asm.GetName();
+			Console.WriteLine($"{aname.Name}, v{aname.Version.Major}.{aname.Version.Minor}.{aname.Version.Build}");
+
+			var exe = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+			bibmarktime = File.GetLastWriteTimeUtc(exe);
+
+			LowercaseFirstWords = args.Contains("-plc");
+			Force = args.Contains("-f");
+			
+			var paths = args.Where(a => !a.StartsWith("-")).ToArray();
+			string path;
+			if (paths.Length == 0)
+			{
+				path = Directory.GetCurrentDirectory();
+				ProcessPath(path);
+			} else
+			{
+				path = paths[0];
+				if (Directory.Exists(path))
+				{
+					ProcessPath(path);
+				}
+				else if (File.Exists(path)) ProcessFile(path);
+			}
+		}
+	}
+}
---
+++ OpenEnglishBible/pandoc/11-Haggai.md
@@ -0,0 +1,31 @@
+## The call to rebuild the Temple
+# 1 
+\bibverse{1} In the second year of Darius the king, on the first day of the sixth month, this message from the Lord came through Haggai the prophet to Zerubbabel the son of Shealtiel, governor of Judah, and to Joshua the son of Jehozadak, the high priest: \bibverse{2} The Lord of hosts says: The people say that the time has not yet come to rebuild the temple of the Lord. 
+
+\bibverse{3} Then this message from the Lord came to Haggai the prophet: \bibverse{4} Is it a time for you yourselves to live in your own well-roofed houses, while this temple lies in ruins? \bibverse{5} The Lord of hosts says: Consider your past experiences. \bibverse{6} You sow much, but bring in little; you eat, but you do not have enough; you drink, but are not filled; you clothe yourselves, but not enough to be warm; and those who earn wages, put those wages in a bag with holes. \bibverse{7} The Lord of hosts says: Consider your experiences. \bibverse{8} Go up to the mountains, and bring wood and rebuild the temple; then I will be pleased with it, and I will reveal my glory. The Lord says: \bibverse{9} You looked for much, and it came to little; and when you brought the harvest home, I blew it away. Why? The Lord of hosts says: Because my temple lies in ruins, while you shelter in your own houses. \bibverse{10} The heavens withhold the dew, and the earth withholds its fruit, \bibverse{11} and I have called forth a drought upon the land and upon the mountains, and upon the grain and the new wine and the oil and upon that which the ground brings forth, and upon people and animals, and upon all the labour of your hands. 
+
+\bibverse{12} Then Zerubbabel the son of Shealtiel and Joshua the son of Jehozadak the high priest, with all the rest of the people, obeyed the command of the Lord their God and the words of Haggai the prophet, as the Lord their God had sent him to them. They showed proper respect for the Lord. \bibverse{13} Haggai, the prophet of the Lord, as the Lord commanded him, told the people: “The Lord says: I am with you.” 
+
+\bibverse{14} And the Lord stirred up the spirit of Zerubbabel the son of Shealtiel, governor of Judah, and the spirit of Joshua the son of Jehozadak the high priest, and the spirit of all the rest of the people, so that they came and worked on the temple of the Lord of hosts, their God, \bibverse{15} in the twenty-fourth day of the sixth month. ## 
+The future glory of the new Temple 
+
+In the second year of Darius the king, 
+
+# 2 
+\bibverse{1} on the twenty-first day of the seventh month, this message from the Lord came to Haggai the prophet, \bibverse{2} telling him to speak to Zerubbabel son of Shealtiel, governor of Judah, and to Joshua, the son of Jehozadak, the high priest, and to all the rest of the people: \bibverse{3} Who is left amongst you that saw this temple in its former glory? And how do you see it now? Doesn't it seem as though there is nothing there? \bibverse{4} The Lord says: Yet now be strong, Zerubbabel, be strong, Joshua, son of Jehozadak, the high priest, be strong, all you people of the land. The Lord says: Begin the work, for I am with you. The Lord of hosts says: \bibverse{5} When your ancestors left Egypt I made a promise to you, and I am still with you. Do not be afraid! \bibverse{6} The Lord of hosts says: In a little while, I will shake the heavens, and the earth, and the sea, and the land. \bibverse{7} And I will shake all nations, and the precious things of all nations shall come here; and I will fill this temple with glory. The Lord of hosts says: \bibverse{8} The silver is mine, and the gold is mine, \bibverse{9} The later glory of this temple shall be greater than the former. The Lord of hosts says: In this place I will grant prosperity. 
+
+This is the message of the Lord of hosts. ## 
+Former displeasure and the new promise of blessings 
+
+\bibverse{10} In the twenty-fourth day of the ninth month, in the second year of Darius, this message from the Lord came to Haggai the prophet: \bibverse{11} The Lord of hosts says: “Ask the priests for a ruling: \bibverse{12} ‘If one carries consecrated meat in the fold of his garment, and with that garment touches bread, or stew, or wine, or oil, or any food, will it become holy?’” 
+
+And the priests answered: “No.” 
+
+\bibverse{13} Then said Haggai, “If one who is unclean by reason of a dead body touches any of these, will it be unclean?” 
+
+And the priests answered: “It would be unclean.” 
+
+\bibverse{14} Haggai replied: “So is this people and so is this nation before me, the Lord declares, and so is every work of their hands; and that which they offer there is unclean. \bibverse{15} Think back from this day, before a stone was laid upon a stone in the temple of the Lord. \bibverse{16} How were you? When you came to a heap of grain expecting twenty measures, there were only ten; when you came to the wine vat to draw out fifty vessels, there were only twenty. \bibverse{17} ‘I struck all the work of your hands with blasting and blight, with mildew and with hail. Yet you didn't turn to me,’ says the Lord, \bibverse{18} ‘Think back from this day, think! From the twenty-fourth day of the ninth month, from the day when the foundations of the temple of the Lord were laid, and consider. \bibverse{19} The seed is in the granary, but the vine and the fig tree and the pomegranate and the olive tree have not yet brought forth fruit. From this day will I bless you.’” ## Zerubbabel
+
+
+\bibverse{20} This message from the Lord came to Haggai a second time on the twenty-fourth day of the month: \bibverse{21} Tell Zerubbabel, governor of Judah: I will shake the heavens and the earth; \bibverse{22} and I will overthrow the throne of kingdoms; and I will destroy the strength of the kingdoms of the nations; and I will overthrow the chariots, and those who ride in them; and the horses and their riders shall come down, each by the sword of his fellow. \bibverse{23} In that day, the Lord of hosts says, I will take you, Zerubbabel, my servant, the son of Shealtiel, the Lord says, and will make you like a signet ring, for I have chosen you, the Lord of hosts says. 
